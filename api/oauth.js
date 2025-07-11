0f4c0b806b4b3ea2c466f0dfef0bb2db34a5f61a
Zunalita <getcyonic+zunalita_admin@gmail.com>
2025-07-11 23:53:04 +0000

    8bacb3dff76eb72a614e2e6681b6275c03614ed9 fix

--- api/oauth.js
+++ api/oauth.js
@@ -1,4 +1,12 @@
 export default async function handler(req, res) {
+  res.setHeader("Access-Control-Allow-Origin", "https://zunalita.github.io");
+  res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+  if (req.method === "OPTIONS") {
+    return res.status(204).end();
+  }
+
   if (req.method !== "POST") {
     return res.status(405).json({ error: "Method not allowed" });
   }
